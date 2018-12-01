8dd0d2558eec0df161515ab0b85730e123c84a1f
duanr1 <Leuchteintraum@gmail.com>
2018-11-30 16:36:22 -0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,27 +1,10 @@
-template(class T>
-void print (TN<T>* t) {  //Preorder, Inorder, Postorder form
-  if (t == nullptr)
-    return;
-  else {
-    //Uncomment for preorder traversals:  std::cout << t->value << std::endl;
-    print(t->left);
-    //Uncomment for inorder traversals:   std::cout << t->value << std::endl;
-    print(t->right);
-    //Uncomment for postorder traversals: std::cout << t->value << std::endl;
-  }
-}
-
-template<class T>
-void print_breadthfirst (TN<T>* t) { //Breadth-First
-  ics::ArrayQueue<TN<T>*> q;
-  q.enqueue(t);                           //Initialize with the root
-
-  while (!q.empty()) {
-    TN<T>* next = q.dequeue();
-    std::cout << next->value << " ";
-    if (next->left != nullptr)     //Only non-nullptr values added
-      q.enqueue(next->left);
-    if (next->right != nullptr)
-      q.enqueue(next->right);
-  }
+void HeapPriorityQueue<T,tgt>::percolate_down(int i) {
+ for (int l = left_child(i); in_heap(l); l = left_child(i)) {
+   int r = right_child(i);
+   int max_child = (!in_heap(r) || gt(pq[l],pq[r]) ? l : r);
+   if ( gt(pq[i],pq[max_child]) )
+      break;
+   std::swap(pq[i],pq[max_child]);
+   i = max_child;
+ }
 }
