df9d9cd02ad6b10f4b3b0ac2185cbaa386e4e241
duanr1 <Leuchteintraum@gmail.com>
2018-11-30 16:31:02 -0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -5,3 +5,19 @@ TN<T>* find (TN<T>* t, T to_find) {
   else
      return find( (to_find < t->value ? t->left : t->right), to_find);
 }
+
+template<class T>
+TN<T>* insert (TN<T>* t, T to_insert) {
+  if (t == nullptr)
+    return new TN<T>(to_insert); //nullptr implicit for left/right subtrees
+  else {
+    if (to_insert < t->value)
+      t->left = insert(t->left, to_insert);
+    else if (to_insert > t->value)
+      t->right = insert(t->right, to_insert);
+  /*else,  for == case
+      ;*/
+
+    return t;
+  }
+}
